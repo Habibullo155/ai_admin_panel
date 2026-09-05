9e528b9a5b83ad21d20bb4dd55fe1f33c3de4b90
Habib <habib@MacBook-Air.local>
2026-09-06 02:45:18 +0300

    add link

--- lib/config.dart
+++ lib/config.dart
@@ -9,6 +9,6 @@
 class AppConfig {
   static const String backendUrl = String.fromEnvironment(
     'BACKEND_URL',
-    defaultValue: 'http://localhost:8000',
+    defaultValue: 'https://16.192.151.160.nip.io',
   );
 }
